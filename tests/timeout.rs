cd0c0925c78fa1792e16b5b4c8117ea79e908086
Stjepan Glavina <stjepang@gmail.com>
2020-08-15 08:18:12 +0200

    Loop

--- tests/timeout.rs
+++ tests/timeout.rs
@@ -8,10 +8,13 @@ fn timeout() -> io::Result<()> {
     let poller = Poller::new()?;
     let mut events = Vec::new();
 
-    let start = Instant::now();
-    poller.wait(&mut events, Some(Duration::from_secs(2)))?;
-    let elapsed = start.elapsed();
+    for _ in 0..5 {
+        let start = Instant::now();
+        poller.wait(&mut events, Some(Duration::from_secs(1)))?;
+        let elapsed = start.elapsed();
 
-    assert!(elapsed >= Duration::from_secs(2));
+        assert!(elapsed >= Duration::from_secs(1));
+    }
+
     Ok(())
 }
