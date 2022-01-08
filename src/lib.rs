055e2711ae572791f834906b74d190823177d67c
Taiki Endo <te316e89@gmail.com>
2022-01-09 02:00:40 +0900

    Ignore clippy::useless_conversion and clippy::unnecessary_cast lints

--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,7 @@
 #![cfg(feature = "std")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
+#![allow(clippy::useless_conversion, clippy::unnecessary_cast)]
 
 use std::fmt;
 use std::io;
